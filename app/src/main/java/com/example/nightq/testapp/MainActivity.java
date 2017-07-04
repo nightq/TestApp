8898feb9088262ec159aaa99d9fc6aa7c79d406c
Mr.NightQ <nightq@WeideMacBook-Pro.local>
2017-07-04 09:48:25 +0800

    动画测试

--- app/src/main/java/com/example/nightq/testapp/MainActivity.java
+++ app/src/main/java/com/example/nightq/testapp/MainActivity.java
@@ -2,28 +2,17 @@ package com.example.nightq.testapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static int dp2pX(int x) {
-        return 3 * x;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final GameOverTransitAnimView gameOverTransitAnimView = (GameOverTransitAnimView)
-                findViewById(R.id.gameOverTransitAnimView);
-        gameOverTransitAnimView.show();
+        ButterKnife.bind(this);
 
-        gameOverTransitAnimView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gameOverTransitAnimView.show();
-            }
-        });
     }
 
 }
---
+++ app/src/main/java/com/example/nightq/testapp/fragmentLib/BaseFragmentManager.java
@@ -0,0 +1,9 @@
+package com.example.nightq.testapp.fragmentLib;
+
+/**
+ * Created by nightq on 2017/6/9.
+ * 使用view 实现类似 fragment 的功能
+ */
+
+public class BaseFragmentManager {
+}
