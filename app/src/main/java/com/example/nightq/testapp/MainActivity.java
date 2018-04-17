516ed279422c7b9ad95534b7e08a68cf209b552c
NightQ <zengwei@wepie.com>
2018-04-17 11:03:07 +0800

    动画测试

--- app/src/main/java/com/example/nightq/testapp/MainActivity.java
+++ app/src/main/java/com/example/nightq/testapp/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.nightq.testapp;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
-import java.util.Random;
+import com.airbnb.lottie.LottieAnimationView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,53 +14,63 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.img1)
-    RoundCornerImageView img1;
-    @BindView(R.id.img2)
-    RoundCornerImageView img2;
-    @BindView(R.id.img3)
-    RoundCornerImageView img3;
-    @BindView(R.id.img4)
-    RoundCornerImageView img4;
-    @BindView(R.id.img5)
-    RoundCornerImageView img5;
-    @BindView(R.id.img6)
-    RoundCornerImageView img6;
-    @BindView(R.id.imgLayout)
-    LinearLayout imgLayout;
+
+    @BindView(R.id.first_charge_dot_lottie_anim)
+    LottieAnimationView firstChargeDotLottieAnim;
+    @BindView(R.id.layout)
+    FrameLayout layout;
+    @BindView(R.id.imageview)
+    ImageView imageview;
+    @BindView(R.id.imageview1)
+    ImageView imageview1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        AnimationDrawable animationDrawable = (AnimationDrawable) imageview.getDrawable();
+        animationDrawable.setOneShot(false);
+        animationDrawable.start();
+        AnimationDrawable animationDrawable1 = (AnimationDrawable) imageview1.getDrawable();
+        animationDrawable1.setOneShot(false);
+        animationDrawable1.start();
     }
 
 
-    @OnClick(R.id.imgLayout)
+    @OnClick(R.id.layout)
     public void onViewClicked() {
-        RoundCornerImageView[] imgs = {
-                img1, img2, img3, img4,
-                img5, img6};
 
-        Random random = new Random();
+        playTitleAnimation();
 
-        for (int i = 0; i < imgs.length; i++) {
+    }
 
-            imgs[i].leftBottomR = 10 + random.nextInt(100);
-            imgs[i].leftTopR = 10 + random.nextInt(100);
-            imgs[i].rightBottomR = 10 + random.nextInt(100);
-            imgs[i].rightTopR = 10 + random.nextInt(100);
-
-            Log.e("nightq", "leftBottomR = " + imgs[i].leftBottomR
-                    + " leftTopR = " + imgs[i].leftTopR
-                    + " rightBottomR = " + imgs[i].rightBottomR
-                    + " rightTopR = " + imgs[i].rightTopR
-            );
-
-            imgs[i].invalidate();
-        }
-        imgLayout.requestLayout();
+    private void playTitleAnimation() {
+//        firstChargeDotLottieAnim.setAnimation("gift.json");
+//        firstChargeDotLottieAnim.setAnimation("first_charge_type_item_animation.json");
+//        firstChargeDotLottieAnim.setAnimation("first_charge_item_line.json");
+        firstChargeDotLottieAnim.setAnimation("first_charge_item_light.json");
+//        firstChargeDotLottieAnim.setSpeed(0.5f);
+//        firstChargeDotLottieAnim.addAnimatorListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
+        firstChargeDotLottieAnim.playAnimation();
     }
 }
