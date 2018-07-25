4adc546c29c41b09c8557aa0884e7bebada9b236
NightQ <zengwei@wepie.com>
2018-07-25 15:14:49 +0800

    验证码

--- app/src/main/java/com/example/nightq/testapp/MainActivity.java
+++ app/src/main/java/com/example/nightq/testapp/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.nightq.testapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.verifyImg)
+    ImageView verifyImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @OnClick(R.id.verifyImg)
+    public void onViewClicked() {
+        verifyImg.setImageBitmap(VerifyFactory.getInstance().createBitmap());
+        Log.e("nightq", "code = " + VerifyFactory.getInstance().getCode());
+    }
 }
